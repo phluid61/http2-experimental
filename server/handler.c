60a89477692d231b15bd1767389ebbb8324e0247
Matthew Kerwin <matthew@kerwin.net.au>
2013-06-17 13:50:00 +1000

    more preprocessor magic

--- server/handler.c
+++ server/handler.c
@@ -15,13 +15,36 @@
 #include "errors.h"
 #include "mystring.h"
 
+#ifdef USE_DRAFT_HEADER
+#  ifdef AC468F3
+/*PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n*/
 const char CONNECTION_HEADER[] = {
-	0x43,0x4f,0x4e,0x20,0x2a,0x20,0x48,0x54,
-	0x54,0x50,0x2f,0x32,0x2e,0x30,0x0d,0x0a,
-	0x0d,0x0a,0x67,0x6f,0x0d,0x0a,0x0d,0x0a};
+	0x50,0x52,0x44,0x20,0x2a,0x20,0x48,0x54,
+	0x54,0x50,0x2f,0x31,0x2e,0x30,0x0d,0x0a,
+	0x0d,0x0a,0x53,0x4d,0x0d,0x0a,0x0d,0x0a};
+#  else
+/*FOO * HTTP/2.0\r\n\r\nBA\r\n\r\n*/
+const char CONNECTION_HEADER[] = {
+	0x46,0x4f,0x4f,0x20,0x2a,0x20,0x48,0x54,
+	0x54,0x50,0x2f,0x31,0x2e,0x30,0x0d,0x0a,
+	0x0d,0x0a,0x42,0x41,0x0d,0x0a,0x0d,0x0a};
+#  endif
+#else
+/*HELO * HTTP/2.0\r\n\r\n.\r\n\r\n*/
+const char CONNECTION_HEADER[] = {
+	0x48,0x45,0x4c,0x4f,0x20,0x2a,0x20,0x48,
+	0x54,0x54,0x50,0x2f,0x32,0x2e,0x30,0x0d,
+	0x0a,0x0d,0x0a,0x2e,0x0d,0x0a,0x0d,0x0a};
+#endif
 const size_t CONNECTION_HEADER_S = sizeof(CONNECTION_HEADER);
 
-/* the following macros assume LE machine */
+#ifdef BIGENDIAN_ARCH
+#  define __network_byte_16(w) (w)
+#  define __network_byte_32(d) (d)
+#else
+#  define __network_byte_16(w) __bswap_16(w)
+#  define __network_byte_32(d) __bswap_32(d)
+#endif
 
 typedef struct frame_header {
 	uint16_t length;
@@ -30,8 +53,8 @@ typedef struct frame_header {
 	uint32_t stream_identifier;
 } frame_header;
 
-#define FRAME_LENGTH(fh) __bswap_16((fh)->length)
-#define SET_FRAME_LENGTH(fh,l) ((fh)->length=__bswap_16(l))
+#define FRAME_LENGTH(fh) __network_byte_16((fh)->length)
+#define SET_FRAME_LENGTH(fh,l) ((fh)->length=__network_byte_16(l))
 
 #define IS_FINAL(fh) ((fh)->flags&1)
 #define SET_FINAL(fh) ((fh)->flags|=1)
@@ -41,8 +64,8 @@ typedef struct frame_header {
 #define SET_CLEAR_PERSISTED(fh) ((fh)->flags|=2)
 #define CLEAR_CLEAR_PERSISTED(fh) ((fh)->flags&=0xFD)
 
-#define STREAM_ID(fh) __bswap_32((fh)->stream_identifier)
-#define SET_STREAM_ID(fh,id) ((fh)->stream_identifier=__bswap_32((id)&0x7FFFFFFF))
+#define STREAM_ID(fh) __network_byte_32((fh)->stream_identifier)
+#define SET_STREAM_ID(fh,id) ((fh)->stream_identifier=__network_byte_32((id)&0x7FFFFFFF))
 
 #define FRAME_TYPE(fh) ((fh)->type)
 #define SET_FRAME_TYPE(fh,t) ((fh)->type=(t))
@@ -66,8 +89,8 @@ typedef struct settings_frame {
 #define SETTINGS_FLAGS(sf) ((sf)->flags)
 #define SET_SETTINGS_FLAGS(sf,f) ((sf)->flags=(f))
 
-#define SETTINGS_ID(sf) ((sf)->id0<<16|__bswap_16((sf)->id1))
-#define SET_SETTINGS_ID(sf,id) {(sf)->id0=((id)>>16)&0xFF;(sf)->id1=__bswap_16((id)&0xFFFF);}
+#define SETTINGS_ID(sf) ((sf)->id0<<16|__network_byte_16((sf)->id1))
+#define SET_SETTINGS_ID(sf,id) {(sf)->id0=((id)>>16)&0xFF;(sf)->id1=__network_byte_16((id)&0xFFFF);}
 
 #define SETTINGS_UPLOAD_BANDWIDTH		1
 #define SETTINGS_DOWNLOAD_BANDWIDTH		2
@@ -75,15 +98,15 @@ typedef struct settings_frame {
 #define SETTINGS_MAX_CONCURRENT_STREAMS	4
 #define SETTINGS_CURRENT_CWND			5
 #define SETTINGS_DOWNLOAD_RETRANS_RATE	6
-#define	SETTINGS_INITIAL_WINDOW_SIZE	7
+#define SETTINGS_INITIAL_WINDOW_SIZE	7
 #define SETTINGS_FLOW_CONTROL_OPTIONS	10
 
 #define IS_PERSISTED(sf) ((sf)->flags&2)
 #define SET_PERSIST(sf) ((sf)->flags|=1)
 #define CLEAR_PERSIST(sf) ((sf)->flags&=0xFE)
 
-#define SETTINGS_VALUE(sf) __bswap_32((sf)->value)
-#define SET_SETTINGS_VALUE(sf,val) ((sf)->value=__bswap_32(val))
+#define SETTINGS_VALUE(sf) __network_byte_32((sf)->value)
+#define SET_SETTINGS_VALUE(sf,val) ((sf)->value=__network_byte_32(val))
 
 #define init_frame_header(fh,t,id) {(fh)->length=0;(fh)->type=(t);(fh)->flags=0;(fh)->stream_identifier=(id);}
 #define init_settings_frame_header(fh) {(fh)->length=sizeof(settings_frame);(fh)->type=frame_SETTINGS;(fh)->flags=0;(fh)->stream_identifier=0;}
