146228412694079cf73dc992a001b46c7412823c
Matthew Kerwin <matthew@kerwin.net.au>
2014-06-07 00:52:41 +1000

    more realistic client and server behaviour

--- ruby-server/http2/client.rb
+++ ruby-server/http2/client.rb
@@ -1,7 +1,7 @@
 
 require 'socket'
 
-require_relative 'connection'
+require_relative 'client_connection'
 
 class HTTP2_Client
 
@@ -15,7 +15,7 @@ class HTTP2_Client
 		raise if @socket
 		@socket = TCPSocket.new @host, @port
 
-		conn = HTTP2_Connection.new(@socket)
+		conn = HTTP2_ClientConnection.new(@socket)
 		conn.max_concurrent_streams = $MAX_CONCURRENT_STREAMS
 		conn.enable_push = false
 		conn.on_message do |stream, headers, data|
@@ -25,25 +25,20 @@ class HTTP2_Client
 				puts "    #{e}"
 			end
 			puts '', data.inspect, ''
+			Thread.main.wakeup
 		end
-		conn.on_pp do |stream, headers|
-			# TODO!
-			puts "<< #{stream}:"
-			headers.each do |e|
-				puts "    #{e}"
-			end
-			puts ''
-		end
-		Thread.abort_on_exception = true
-		t = conn.start_client
+		t = conn.start
 
+		puts "GET /"
 		conn.begin_headers(2, end_stream: true) do |out|
 			out.send ':method', 'GET'
 			out.send ':scheme', 'http'
 			out.send ':path', '/'
 			out.send ':authority', 'www.example.com'
 		end
+		Thread.stop
 
+		puts "POST /"
 		conn.begin_headers(4) do |out|
 			out.flush_reference_set
 			out.send ':method', 'POST'
@@ -52,11 +47,15 @@ class HTTP2_Client
 			out.send ':authority', 'www.example.com'
 		end
 		conn.send_data(4, 'foo', end_stream: true)
+		Thread.stop
 
 		# TODO: work out how to ping/pong
 
+		puts "Goodbye"
 		conn.goaway :NO_ERROR, debug_data: 'This is the end... beautiful friend'
+		sleep 0.1
 
+		conn.shutdown
 		t.join
 	end
 
--- ruby-server/http2/server.rb
+++ ruby-server/http2/server.rb
@@ -2,7 +2,7 @@
 require 'socket'
 require 'threadpuddle'
 
-require_relative 'connection'
+require_relative 'server_connection'
 
 class HTTP2_Server
 
@@ -16,9 +16,10 @@ class HTTP2_Server
 		@tp = ThreadPuddle.new 100
 
 		srv = TCPServer.new @port
+		STDERR.puts "[#{Time.now}] listening on #{@port}"
 		loop {
 			@tp.spawn(srv.accept) do |cl|
-				conn = HTTP2_Connection.new(cl)
+				conn = HTTP2_ServerConnection.new(cl)
 				conn.max_concurrent_streams = $MAX_CONCURRENT_STREAMS
 				conn.on_message do |stream, headers, data|
 					# TODO!
@@ -27,12 +28,37 @@ class HTTP2_Server
 						puts "    #{e}"
 					end
 					puts '', data.inspect
+
+					m = headers.find{|e|e.name==':method'}
+					if m && m.value == 'GET'
+						conn.begin_headers(stream+1) do |out|
+							out.flush_reference_set
+							out.send ':status', '200'
+							out.send 'content-type', 'text/plain'
+						end
+						conn.send_data(stream+1, 'Ok', end_stream: true)
+					elsif m
+						conn.begin_headers(stream+1) do |out|
+							out.flush_reference_set
+							out.send ':status', '501'
+							out.send 'content-type', 'text/plain'
+						end
+						conn.send_data(stream+1, 'Not Implemented', end_stream: true)
+					else
+						conn.begin_headers(stream+1) do |out|
+							out.flush_reference_set
+							out.send ':status', '400'
+							out.send 'content-type', 'text/plain'
+						end
+						conn.send_data(stream+1, 'Bad Request', end_stream: true)
+					end
+
 				end
 				conn.on_pp do |stream, headers|
 					# Nope, don't accept these
 					conn.goaway( :PROTOCOL_ERROR, debug_data: "clients can't push" )
 				end
-				conn.start_server
+				conn.start
 			end
 		}
 	end
