995abfdbc9e72b6564ba4ab9b52f09baf1ff7535
Matthew Kerwin <matthew@kerwin.net.au>
2014-08-08 13:04:23 +1000

    change library to use mk-hpack project

--- hpack-gem/hpack.c
+++ hpack-gem/hpack.c
@@ -1,13 +1,11 @@
 
-#include "../hpack/huffman.c"
+#include <hpack.h>
 
 #include "ruby.h"
 
 #define BUFFER_SIZE (1024*16)
 
 VALUE HPACK = Qnil;
-VALUE encode_mutex = Qnil;
-VALUE decode_mutex = Qnil;
 
 void Init_hpack();
 
@@ -15,8 +13,6 @@ VALUE rb_huffman_encode(VALUE self, VALUE str);
 VALUE rb_huffman_decode(VALUE self, VALUE str);
 
 void Init_hpack() {
-	encode_mutex = rb_mutex_new();
-	decode_mutex = rb_mutex_new();
 	HPACK = rb_define_module("HPACK");
 	rb_define_singleton_method(HPACK, "huffman_encode", rb_huffman_encode, 1);
 	rb_define_singleton_method(HPACK, "huffman_decode", rb_huffman_decode, 1);
@@ -30,10 +26,10 @@ VALUE rb_huffman_encode(VALUE self, VALUE str) {
 
 	Check_Type(str, T_STRING);
 
-	rb_mutex_lock(encode_mutex);
-	n = huffman_encode((unsigned char*)RSTRING_PTR(str),(size_t)RSTRING_LEN(str), buf,BUFFER_SIZE);
-	e = huffman_encoder_error;
-	rb_mutex_unlock(encode_mutex);
+	e = huffman_encode(
+			(unsigned char*)RSTRING_PTR(str), (size_t)RSTRING_LEN(str), NULL,
+			buf, BUFFER_SIZE, &n
+	);
 	if (e) {
 		rb_raise(rb_eRuntimeError, "Huffman encoder error %d", e);
 	}
@@ -50,10 +46,10 @@ VALUE rb_huffman_decode(VALUE self, VALUE str) {
 
 	Check_Type(str, T_STRING);
 
-	rb_mutex_lock(decode_mutex);
-	n = huffman_decode((unsigned char*)RSTRING_PTR(str),(size_t)RSTRING_LEN(str), buf,BUFFER_SIZE);
-	e = huffman_decoder_error;
-	rb_mutex_unlock(decode_mutex);
+	e = huffman_decode(
+			(unsigned char*)RSTRING_PTR(str), (size_t)RSTRING_LEN(str), NULL,
+			buf, BUFFER_SIZE, &n
+	);
 	if (e) {
 		rb_raise(rb_eRuntimeError, "Huffman decoder error %d", e);
 	}
