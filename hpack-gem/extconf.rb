8ca593c0ba418cb5102d493d3743788fb19a3a29
Matthew Kerwin <matthew@kerwin.net.au>
2014-08-08 13:04:23 +1000

    change library to use mk-hpack project

--- hpack-gem/extconf.rb
+++ hpack-gem/extconf.rb
@@ -1,5 +1,20 @@
 require 'mkmf'
-ext_name = 'hpack'
-dir_config(ext_name)
-create_makefile(ext_name)
+
+$MYLIBDIR = Dir.pwd+'/mk-hpack'
+$MYLIBNAME = %(hpack)
+
+INCLUDE_DIR = RbConfig::CONFIG['includedir']
+LIBDIR      = RbConfig::CONFIG['libdir']
+
+dir_config('hpack', [INCLUDE_DIR], [LIBDIR, $MYLIBDIR])
+
+unless find_header('hpack.h', $MYLIBDIR)
+	abort 'mk-hpack is missing (1)'
+end
+
+unless find_library($MYLIBNAME, 'huffman_encode')
+	abort 'mk-hpack is missing (2)'
+end
+
+create_makefile('hpack')
 
